2a4a26df6e85d61026e04426ad13779065edbe5e
mario-iml <mario@immel-eich.de>
2026-01-21 11:16:55 +0100

    add hyprlock

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -43,3 +43,10 @@ set -gx PATH $PATH /Users/mario/.lmstudio/bin
 fish_add_path /Users/mario/.antigravity/antigravity/bin
 
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+# pnpm
+set -gx PNPM_HOME "/home/mario/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
